2f3d790dc9dac48802f113dd084b5de2bb21bfa1
afrotonder <o.rosado18@gmail.com>
2021-12-12 03:01:13 -0400

    Remove unnecessary stuff. Move necessary files to main folder. Restructure everything so its simple.

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-// import Typed from "typed.js";
-// not going to use this
-// // ---------- HEADER ----------
-// let headText = ["Web Dev", "Hot Dogs"];
-
-// const options = {
-//   strings: [headText[0]],
-//   typeSpeed: 100,
-//   loop: "true"
-// };
-
-// const options1 = {
-//   strings: [headText[1]],
-//   typeSpeed: 100,
-//   loop: "true"
-// };
-import './src/styles.css';
-import linkedin_icon from './images/in-icon.png' ;
-import halloween2018 from './images/halloween-2018.png' ;
-import js_icon from './images/js-icon.png' ;
-import css_icon from './images/css-icon.png' ;
-import html_icon from './images/html-icon.png' ;
-import ts_icon from './images/tys-icon.png' ;
-import sql_icon from './images/sql-icon.png' ;
-import xcode_icon from './images/xcode-icon.png' ;
-import android_icon from './images/android.png' ;
-import chess_icon from './images/chesscomicon.png' ;
-import twitter_icon from './images/twittericon.png' ;
-import python_icon from './images/py-icon.png' ;
-import wordpress_icon from './images/wordpress-icon.png' ;
-import octocat_icon from './images/octocat.png' ;
-import php_icon from './images/php-icon.png' ;
-import angular_icon from './images/angular-icon.png' ;
-import ionic_icon from './images/ionic-icon.png' ;
-import capacitor_icon from './images/capacitor-icon.png' ;
-import bootstrap_icon from './images/bootstrap-icon.png' ;
-// import  from './images/.png' ;
-
-
-
-
-const icons = require('./icons.json');
-
-console.log('icons ', icons)
-// import * as './assets' ;
-// const typedHeader = new Typed(".header-text", options);
-// const typedHeader1 = new Typed(".ht1", options1);
-
-// ---------- END HEADER ----------
-console.log("GOJIRAA TESTING");
 // ---------- SET AVATAR DATA ----------
 const uname = "Omar Rosado Ramirez";
 const sm_uname = "O. Rosado Ramirez";
@@ -61,82 +11,70 @@ document
   .getElementById("avatar")
   .setAttribute(
     "src",
-    "https://pbs.twimg.com/profile_images/1087372828473733120/eJJHI1Su_400x400.jpg"
+    "https://scontent-sju1-1.xx.fbcdn.net/v/t1.6435-9/50887190_2300320363577374_8314854031319105536_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=KXuF4cUjkogAX_LyrPc&_nc_ht=scontent-sju1-1.xx&oh=65d5f58e208e64cf46733a421320b3a5&oe=61DBC289"
   );
 
-document.getElementsByClassName("header-text")[0].innerHTML = uname;
 
-document.getElementsByClassName("header-text ht1")[0].innerHTML = slogan;
-
-document.getElementById("meet").innerHTML = ` I'm a software developer with 6+ years of experience
-                                              creating apps and sites in a wide variety of stacks. 
-                                              I'm currently a software developer @<a href="https://tuchequera.com" target="_blank">${job1}</a> 
-                                              and a Business Intelligence Analyst @<a href="https://apshealth.com/" taget="_blank">
-                                              ${job2}</a>.`;
+document.getElementById("meet").innerHTML = `🍕 I'm currently working as a:
+                                              <ul class="jobList">
+                                                <li class="job">software developer @ <a href="https://tuchequera.com" target="_blank">${job1}</a> </li>
+                                                <li class="job">Business Intelligence Analyst @<a href="https://apshealth.com/" taget="_blank">${job2}</a>. </li>
+                                              </ul>`
+                                               
+                                               ;
 // ---------- END SET AVATAR DATA ----------
 console.log('');
 
 // ---------- ICON DATA----------
 let imgs = [
-  // {
-  //   name: "ig",
-  //   url: "https://www.instagram.com/afrotonder/?hl=es-la",
-  //   title: "afrotonders Instagram",
-  //   alt: "Instagram Icon",
-  //   source: "assets/instagram-icon.png"
-  // },
   {
     name: "lin",
     url: "linkedin.com/in/afrotonder/",
     title: "afrotonder@LinkedIn",
     alt: "LinkedIn Icon",
-    source: linkedin_icon 
+    source: "https://brandlogos.net/wp-content/uploads/2016/06/linkedin-logo-512x512.png" 
   },
   {
     name: "grm",
     url: "https://angular-g2qjyv.stackblitz.io/",
     title: "TheGripRepo",
     alt: "Rob Zombies Halloween 2018 Pumpkin Icon",
-    source:  halloween2018 
+    source:  "https://trickortreatstudios.com/media/catalog/product/cache/1da4909b8e3ea5eea17a9fb4c6e4a516/h/a/halloween2018-enamelpin_1.png" 
   },
   {
     name: "github",
     url: "https://github.com/afrotonder",
     title: "afrotonder@GitHub",
     alt: "Github Octocat Logo",
-    source: octocat_icon 
+    source: "https://github.githubassets.com/images/modules/logos_page/Octocat.png" 
   },
   {
     name: "chess",
     url: "https://www.chess.com/member/afrotonder",
     title: "afrotonder@Chess.com",
     alt: "Chess.com Icon",
-    source: chess_icon 
+    source: "https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/SamCopeland/phpmeXx6V.png" 
   },
   {
     name: "twt",
     url: "https://twitter.com/afrotonder",
     title: "afrotonder@Twitter",
     alt: "Twitter Icon",
-    source: twitter_icon 
+    source: "https://icons-for-free.com/iconfiles/png/512/identity+logo+new+twitter+icon-1320161424699291885.png" 
   }
 ];
 
 let tools = [
-  { name: "JS (ES6+)", source: js_icon},
-  { name: "HTML", source: html_icon},
-  { name: "CSS/SCSS", source: css_icon },
-  { name: "TypeScript", source: ts_icon },
-  { name: "Angular", source: angular_icon },
-  { name: "Ionic", source: ionic_icon },
-  { name: "iOS", source: xcode_icon },
-  { name: "Android", source: android_icon },
-  { name: "Capacitor", source: capacitor_icon },
-  { name: "Python", source: python_icon },
-  { name: "PHP", source: php_icon },
-  { name: "Bootstrap", source: bootstrap_icon },
-  { name: "SQL", source: sql_icon },
-  { name: "WordPress", source: wordpress_icon }
+  { name: "JS (ES6+)", source: "./images/js-icon.png"},
+  { name: "HTML", source: "./images/html-icon.png"},
+  { name: "CSS/SCSS", source: "./images/css-icon.png" },
+  { name: "Angular", source: "./images/angular-icon.png" },
+  // { name: "Ionic", source: "./images/ionic.jpg" },
+  // { name: "iOS", source: xcode_icon },
+  { name: "Ionic", source: "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ionic-512.png" },
+  // { name: "Android", source: android_icon },
+  // { name: "Capacitor", source: "./images/capacitor-icon.png" },
+  { name: "Capacitor", source: "https://seeklogo.com/images/C/capacitor-logo-DF3634DD70-seeklogo.com.png" },
 ];
 
 // set/ renderfooter
@@ -145,6 +83,7 @@ for (let img of imgs) {
     console.log('o,g ', img);
   elem.setAttribute("href", img.url);
   elem.setAttribute("target", "_blank");
+  elem.setAttribute("class", "toolIcon");
 
   elem.innerHTML =
     `<img src=` +
@@ -178,24 +117,7 @@ for (let tool of tools) {
   document.getElementById("tools").appendChild(elem);
 }
 
-// const rightButton = document.getElementById("scroll-right");
 
-document.getElementById("scroll-right").onclick = function () {
-  document.getElementById("tools").scrollLeft += 500;
-};
-
-// const leftButton = document.getElementById("scroll-left");
-
-document.getElementById("scroll-left").onclick = function () {
-  document.getElementById("tools").scrollLeft += -500;
-};
-// --------------- set Description --------------- //
-
-// set normal desc
-
-// set mobile desc
-
-// --------------- set Description --------------- //
 
 // --------------- set footer data --------------- //
 const footer = "afrotonder © ";
